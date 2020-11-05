df04c901715afe121fa687d73ae30b8c9c12529b
vasyascheg <61463277+vasyascheg@users.noreply.github.com>
2020-11-06 00:10:53 +0300

    Create task_11_2.py

---
+++ 11_modules/task_11_2.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 11.2
+
+Создать функцию create_network_map, которая обрабатывает
+вывод команды show cdp neighbors из нескольких файлов и объединяет его в одну общую топологию.
+
+У функции должен быть один параметр filenames, который ожидает как аргумент список с именами файлов, 
+в которых находится вывод команды show cdp neighbors.
+
+Функция должна возвращать словарь, который описывает соединения между устройствами.
+Структура словаря такая же, как в задании 11.1:
+    {("R4", "Fa0/1"): ("R5", "Fa0/1"),
+     ("R4", "Fa0/2"): ("R6", "Fa0/0")}
+
+
+Cгенерировать топологию, которая соответствует выводу из файлов:
+* sh_cdp_n_sw1.txt
+* sh_cdp_n_r1.txt
+* sh_cdp_n_r2.txt
+* sh_cdp_n_r3.txt
+
+В словаре, который возвращает функция create_network_map, не должно быть дублей.
+
+С помощью функции draw_topology из файла draw_network_graph.py нарисовать схему на основании топологии, полученной с помощью функции create_network_map.
+Результат должен выглядеть так же, как схема в файле task_11_2a_topology.svg
+
+
+При этом:
+* Расположение устройств на схеме может быть другим
+* Соединения должны соответствовать схеме
+
+Не копировать код функций parse_cdp_neighbors и draw_topology. Если функция parse_cdp_neighbors не может обработать вывод
+одного из файлов с выводом команды, надо исправить код функции в задании 11.1.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+> Для выполнения этого задания, должен быть установлен graphviz:
+> apt-get install graphviz
+
+> И модуль python для работы с graphviz:
+> pip install graphviz
+
+"""
+
+# эти заготовки написаны чтобы показать в какой момент должна
+# рисоваться топология (после вызова функции)
+from task_11_1 import parse_cdp_neighbors
+from draw_network_graph import draw_topology
+def create_network_map(filenames):
+    d = {}
+    for fn in filenames:
+        with open(fn) as f:  
+            t = parse_cdp_neighbors(f.read())
+            d.update(t) #складываем все в один словарь
+    l = {} #удаление дублей
+    for k, v in d.items(): 
+        #if not ((k in l.keys() and l[k] == v) or (v in l.keys() and l[v] == k)): 
+        if not (v in l.keys() and l[v] == k):    
+            l[k] = v
+
+    return l          
+
+
+
+if __name__ == "__main__":
+    infiles = [
+        "sh_cdp_n_sw1.txt",
+        "sh_cdp_n_r1.txt",
+        "sh_cdp_n_r2.txt",
+        "sh_cdp_n_r3.txt",
+    ]
+
+    topology = create_network_map(infiles)
+    #print(topology)
+    # рисуем топологию:
+    draw_topology(topology)
+    
+    

18188171cbe342b4d06076b0b8380aee0da3d05a
vasyascheg <61463277+vasyascheg@users.noreply.github.com>
2020-11-06 00:08:01 +0300

    Create task_9_3.py

---
+++ 09_functions/task_9_3.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 9.3
+
+Создать функцию get_int_vlan_map, которая обрабатывает конфигурационный файл коммутатора
+и возвращает кортеж из двух словарей:
+* словарь портов в режиме access, где ключи номера портов, а значения access VLAN (числа):
+{'FastEthernet0/12': 10,
+ 'FastEthernet0/14': 11,
+ 'FastEthernet0/16': 17}
+
+* словарь портов в режиме trunk, где ключи номера портов, а значения список разрешенных VLAN (список чисел):
+{'FastEthernet0/1': [10, 20],
+ 'FastEthernet0/2': [11, 30],
+ 'FastEthernet0/4': [17]}
+
+У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
+
+Проверить работу функции на примере файла config_sw1.txt
+
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+"""
+def get_int_vlan_map(config_filename):
+    '''
+    У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
+    '''
+    access = {}
+    trunk = {}
+    f = open(config_filename, 'r')
+    k = ''
+    for i in f.read().split('\n'):
+        if i.startswith('interface'):
+            k = i.split(' ')[1]    
+        if i.startswith(' switchport access vlan'):
+            access[k]= int(i.split(' ')[-1])    
+        if i.startswith(' switchport trunk allowed vlan'):
+            trunk[k]=[int(x) for x in i.split(' ')[-1].split(',')]     
+
+
+    f.close()
+    return access, trunk
+print(get_int_vlan_map('config_sw1.txt'))

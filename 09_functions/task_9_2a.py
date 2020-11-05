10a1e138969378254a49af67d7abc32d1473b4e8
vasyascheg <61463277+vasyascheg@users.noreply.github.com>
2020-11-06 00:07:17 +0300

    Create task_9_2a.py

---
+++ 09_functions/task_9_2a.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 9.2a
+
+Сделать копию функции generate_trunk_config из задания 9.2
+
+Изменить функцию таким образом, чтобы она возвращала не список команд, а словарь:
+    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
+    - значения: список команд, который надо выполнить на этом интерфейсе
+
+Проверить работу функции на примере словаря trunk_config и шаблона trunk_mode_template.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+"""
+
+
+trunk_mode_template = [
+    "switchport mode trunk",
+    "switchport trunk native vlan 999",
+    "switchport trunk allowed vlan",
+]
+
+trunk_config = {
+    "FastEthernet0/1": [10, 20, 30],
+    "FastEthernet0/2": [11, 30],
+    "FastEthernet0/4": [17],
+}
+
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    '''
+    Генерирует конфигурацию для trunk-портов.
+    Параметры:
+- intf_vlan_mapping: ожидает как аргумент словарь с соответствием интерфейс-VLANы такого вида:
+    {'FastEthernet0/1': [10, 20],
+     'FastEthernet0/2': [11, 30],
+     'FastEthernet0/4': [17]}
+- trunk_template: ожидает как аргумент шаблон конфигурации trunk-портов в виде списка команд (список trunk_mode_template)
+    '''
+    res = {}
+    for k, v in intf_vlan_mapping.items():
+        a = trunk_template.copy()
+        a[2] = a[2]+' '+str(v).strip('[ ]').replace(' ', '')
+        res[k] = a
+
+    return res
+
+print('Проверка работы функции на примере словаря trunk_config и списка команд trunk_mode_template:')
+print( generate_trunk_config(trunk_config, trunk_mode_template))
